dd362ff6586ca86311fd80b3d3d465a00a8ee62a
sharnkonet <sharnkonet.reitsma@gmail.com>
2022-01-15 00:23:09 +1300

    Macro successfully runs but unable to get the generated function to work correctly

--- interface.jl
+++ interface.jl
@@ -23,12 +23,36 @@ struct ODEFunction <: Function
 end
 
 
+# """ 
+#     ODEFunc!(t, u, [constants...])
 
+# Update u based on current position and system defined using @ODE and provided
+# constants.
+
+# # Example
+# ``` julia-repl
+# julia> lorenz = @ODE quote
+#     dx = σ*(y-x) 
+#     dy = x*(ρ-z) - y 
+#     dz = x * y - β*z
+# end
+
+# julia> u = [
+#     [1,2,3]
+#     [2,3,4]
+#     [4,5,6]
+# ]
+
+# julia> lorenz(0.1, u, σ = 10, ρ = 28, β = 8/3)
+# ```
+# """
 macro ODE(∂::Vararg{Expr})
+
     dim = length(∂)
+    @assert dim > 0 "Missing input expression(s)."
 
     # Check that ∂ is not used within the code
-    @assert nor([occursin("∂", string(expr)) for expr in [∂x, ∂y, ∂z]]...) "Remove usage of ∂."
+    @assert nor([occursin("∂", string(expr)) for expr in ∂]...) "Remove usage of ∂."
             
     symbols = Vector{Symbol}()
 
@@ -38,54 +62,32 @@ macro ODE(∂::Vararg{Expr})
 
     symbols = filter!(!Meta.isoperator, symbols)
     symbols = filter!(!isFunction, symbols)
-
-    diff_symbols = Set{Symbol}([expr.args[1] for expr in ∂])
-    dependent_vars = Set{Symbol}([Symbol(string(symbol)[end]) for symbol in diff_symbols])
+ 
+    diff_symbols = Vector{Symbol}([expr.args[1] for expr in ∂])
+    dependent_vars = Vector{Symbol}([Symbol(string(symbol)[end]) for symbol in diff_symbols])
     constants = setdiff(symbols, diff_symbols, dependent_vars)
     constants = [Expr(:(::), constant, :(Real)) for constant in constants]
 
-    @assert length(dependent_vars) == dim
+    @assert length(dependent_vars) <= dim
 
     quote
-        """ 
-            ODEFunc!(t, u, [constants...])
+        function ODEFunc(t::Real, u::Matrix{<:Real}; $(constants...))
 
-        Update u based on current position and system defined using @ODE and provided
-        constants.
+            # @assert size(u, 1) == $dim
 
-        # Example
-        ``` julia-repl
-        julia> lorenz = @ODE quote
-            dx = σ*(y-x) 
-            dy = x*(ρ-z) - y 
-            dz = x * y - β*z
-        end
-
-        julia> u = [
-            [1,2,3]
-            [2,3,4]
-            [4,5,6]
-        ]
-
-        julia> lorenz(0.1, u, σ = 10, ρ = 28, β = 8/3)
-        ```
-        """
-        function ODEFunc!(t::Real, u::Matrix{<:Real}; $(constants...))
-
-            @assert size(u, 1) == $dim
-
-            $(dependent_vars...) = eachrow(u)
-
-            for equation in ∂
-                $(@.(equation))
-            end
+            x, y, z = eachrow(u)
+            
+            $(Expr(∂[1]))
+            $(∂[2])
+            $(∂[3])
         end
 
         ODEFunction(ODEFunc, $dim)
-    end 
+    end
 end
 
-macro ODE(derivatives)
+# Method handles quote blocks being used to define ODEs
+macro ODE(derivatives::Expr)
     derivatives = filter(x -> !(x isa LineNumberNode), derivatives.args)
     # @assert length(derivatives) == 3 "Incorrected number of derivatives defined."
     @eval @ODE($(derivatives...))
