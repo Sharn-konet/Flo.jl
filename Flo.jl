fabdd19884058412411ad98d800aa7fb2f9146ce
sharnkonet <sharnkonet.reitsma@gmail.com>
2022-01-26 12:50:46 +1300

    Create function which determines the correct limits to plot on

--- Flo.jl
+++ Flo.jl
@@ -2,6 +2,8 @@ using Revise
 
 using GLMakie
 using Makie.Colors
+using LinearAlgebra: norm
+using ElasticArrays
 
 include("Swarms.jl")
 include("ExampleFunctions.jl")
@@ -11,10 +13,14 @@ using .Swarms: Swarm, step!, RK4
 
 const FPS = 144
 
-attractor = Swarm(lorenz)
+ode_func = lorenz
+attractor = Swarm(ode_func)
 
-function createFigure(fig::Makie.Figure = Figure())
-    ax = Axis3(fig[1,1], protrusions = (0, 0, 0, 0), viewmode = :fit,  aspect = :data)
+function createFigure(; fig::Makie.Figure = Figure(), limits)
+
+    include("Theme.jl")
+
+    ax = Axis3(fig[1,1], aspect = (1,1,1), autolimitaspect = true, limits = limits)
 
     fig[2,1] = buttongrid = GridLayout(tellwidth = false)
     sim_state = Observable("Run")
@@ -55,7 +61,33 @@ function createSliders!(fig, parameters::NamedTuple)
     end
 end
 
-figure, run_var = createFigure(); figure
+function findLimits(func::Function)::Tuple
+    attractor = Swarm(func, size = 2, positions = (rand(Float64, 3, 2) .-1) .* 1e-6)
+    prev_positions = attractor.positions
+    history = ElasticArray{Float64}(undef, 3, 2, 0)
+    append!(history, prev_positions)
+    step!(attractor, RK4)
+    while norm(attractor.positions[:,1] .- attractor.positions[:,2]) < 60
+        prev_positions = attractor.positions
+        append!(history, prev_positions)
+        step!(attractor, RK4)
+    end
+
+    maxInHistory = history -> maximum(history, dims = 3)
+    minInHistory = history -> minimum(history, dims = 3)
+
+    maxInSwarm = swarm_positions -> maximum(swarm_positions, dims = 2)
+    minInSwarm = swarm_positions -> minimum(swarm_positions, dims = 2)
+
+    upper_limits = (maxInSwarm ∘ maxInHistory)(history)
+    lower_limits = (minInSwarm ∘ minInHistory)(history)
+
+    limits = Tuple([(zip(lower_limits, upper_limits)...)...]) .* 1.2
+end
+
+limits = findLimits(ode_func)
+
+figure, run_var = createFigure(limits = limits); figure
 
 # createSliders!(figure, (σ = 10, ρ = 28, β = 8/3))
 
@@ -63,17 +95,17 @@ solver = RK4
 
 points = Observable(Point3f.(Vector{Float64}.([eachcol(attractor.positions)...])))
 
-scatter!(points, markersize = 2000)
+scatter!(points, markersize = 0.05, markerspace = SceneSpace)
 
-while true
+while events(figure.scene).window_open.val
     # Main sim loop
-    while run_var[]
+    while run_var[] & events(figure.scene).window_open.val
         step!(attractor, solver)
         points[] = [eachcol(attractor.positions)...]
         notify(points)
-        sleep(1/FPS)
+        sleep(1/FPS - 0.000208)
     end
-    sleep(0.0001)
+    sleep(1e-12)
 end
 
 
